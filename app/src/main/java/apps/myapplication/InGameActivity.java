190d8714ddaba09aefd0ccbd1b3d70d11f4ed0dd
Tim Buckers <tbuckers@student.tudelft.nl>
2015-08-19 17:04:56 +0200

    homescreen done

--- app/src/main/java/apps/myapplication/InGameActivity.java
+++ app/src/main/java/apps/myapplication/InGameActivity.java
@@ -1,26 +1,23 @@
 package apps.myapplication;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 
-public class HomeScreenActivity extends AppCompatActivity {
+/**
+ * Created by Tim on 8/19/2015.
+ */
+public class InGameActivity extends ActionBarActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
-
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-        if(displayMetrics.heightPixels / displayMetrics.widthPixels < 1){
-            imageView.setImageDrawable(getResources().getDrawable(R.drawable.homescreen_background));
-        }else{
-            imageView.setImageDrawable(getResources().getDrawable(R.drawable.homescreen_background_long));
-        }
+        setContentView(R.layout.activity_in_game);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
 
     }
 
