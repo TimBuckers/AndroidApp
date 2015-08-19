37d66f94aae4c25fc3a587224d1f287b5f778cf8
Boning Gong <bgong@student.tudelft.nl>
2015-08-19 22:02:19 +0200

    Yay

---
+++ app/src/main/java/apps/myapplication/LevelOneFragment.java
@@ -0,0 +1,26 @@
+package apps.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by Boning on 19-8-2015.
+ */
+public class LevelOneFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_level_one_fragment, container, false);
+        return rootView;
+    }
+}
--- app/src/main/java/apps/myapplication/SelectLevelActivity.java
+++ app/src/main/java/apps/myapplication/SelectLevelActivity.java
@@ -1,13 +1,20 @@
 package apps.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class SelectLevelActivity extends ActionBarActivity {
     private Toolbar toolbar;
+
+    public void inGame(View view) {
+        Intent intent = new Intent(this, InGameActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
